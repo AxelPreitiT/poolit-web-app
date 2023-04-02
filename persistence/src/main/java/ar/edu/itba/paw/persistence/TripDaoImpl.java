a4dc10e7b1c939002d9f3910e70069185120219f
Axel Facundo Preiti Tasat <apreititasat@itba.edu.ar>
2023-04-02 15:42:01 -0300

    feat: Implement TripDao and TripService

---
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/TripDaoImpl.java
@@ -0,0 +1,14 @@
+package ar.edu.itba.paw.persistence;
+
+import ar.edu.itba.paw.interfaces.persistence.TripDao;
+import ar.edu.itba.paw.models.Trip;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TripDaoImpl implements TripDao {
+
+    @Override
+    public Trip create(final String originCity, final String originAddress, final String destinationCity, final String destinationAddress, final String date, final String time, final int seats, final String email, final String phone) {
+        return new Trip(originCity, originAddress, destinationCity, destinationAddress, date, time, seats, email, phone);
+    }
+}
