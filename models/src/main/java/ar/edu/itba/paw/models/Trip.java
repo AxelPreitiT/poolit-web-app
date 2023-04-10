2d1e1c86cce177cf6f408c44e0c8ac32eed55da2
JoseMenta <jmentasti@itba.edu.ar>
2023-04-10 20:47:33 -0300

    fix: change Trip model to use LocalDateTime and avoid combining DAO's

--- models/src/main/java/ar/edu/itba/paw/models/Trip.java
+++ models/src/main/java/ar/edu/itba/paw/models/Trip.java
@@ -2,6 +2,7 @@ package ar.edu.itba.paw.models;
 
 import java.sql.Date;
 import java.sql.Time;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -9,27 +10,23 @@ import java.util.Objects;
 public class Trip {
     private final City originCity, destinationCity;
     private final String originAddress, destinationAddress;
-    private final Date date;
-    private final Time time;
+    private final LocalDateTime dateTime;
     private final Car car;
     private final User driver;
     private final int maxSeats;
     private final long tripId;
-    private int occupiedSeats;
-    private final List<User> passengers;
-    public Trip(final long tripId,final City originCity, final String originAddress, final City destinationCity, final String destinationAddress, final Date date, final Time time, final int maxSeats, User driver, Car car, List<User> passengers) {
+    private final int occupiedSeats;
+    public Trip(final long tripId,final City originCity, final String originAddress, final City destinationCity, final String destinationAddress, final LocalDateTime dateTime, final int maxSeats, final User driver, final Car car, final int occupiedSeats) {
         this.originCity = originCity;
         this.originAddress = originAddress;
         this.destinationCity = destinationCity;
         this.destinationAddress = destinationAddress;
-        this.date = date;
-        this.time = time;
+        this.dateTime = dateTime;
         this.maxSeats = maxSeats;
         this.tripId = tripId;
         this.driver = driver;
         this.car = car;
-        this.passengers = passengers;
-        this.occupiedSeats = passengers.size();
+        this.occupiedSeats = occupiedSeats;
     }
     public City getOriginCity() {
         return originCity;
@@ -54,14 +51,9 @@ public class Trip {
         return car;
     }
 
-    public Date getDate() {
-        return date;
+    public LocalDateTime getDateTime() {
+        return dateTime;
     }
-
-    public Time getTime() {
-        return time;
-    }
-
     public int getMaxSeats() {
         return maxSeats;
     }
@@ -74,7 +66,4 @@ public class Trip {
         return occupiedSeats;
     }
 
-    public List<User> getPassengers() {
-        return passengers;
-    }
 }
