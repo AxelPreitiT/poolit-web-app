8b7dbe014c62615ace028e48f30212a03c7c1a86
JoseMenta <jmentasti@itba.edu.ar>
2023-04-14 15:48:00 -0300

    refactor: change method to create query string to reuse them and add conditions dynamically

--- persistence/src/main/java/ar/edu/itba/paw/persistence/TripDaoImpl.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/TripDaoImpl.java
@@ -101,11 +101,10 @@ public class TripDaoImpl implements TripDao {
 
     @Override
     public List<Trip> getFirstNTrips(long n){
-        return jdbcTemplate.query("SELECT trips.trip_id, trips.max_passengers, trips.origin_date_time, trips.origin_address, origin.name as origin_city_name, origin.city_id as origin_city_id, origin.province_id as origin_province_id, trips.destination_address, destination.name as destination_city_name, destination.city_id as destination_city_id, destination.province_id as destination_province_id, users.email as user_email, users.user_id as user_id, users.phone as user_phone, cars.car_id as car_id, cars.plate  as car_plate, cars.info_car as car_info_car, count(passengers.user_id) as occupied_seats\n" +
-                "FROM trips NATURAL JOIN trips_cars_drivers NATURAL JOIN users NATURAL JOIN cars JOIN cities origin ON trips.origin_city_id = origin.city_id JOIN cities destination ON trips.destination_city_id=destination.city_id LEFT OUTER JOIN passengers ON passengers.trip_id = trips.trip_id\n" +
-                "GROUP BY trips.trip_id, trips.origin_date_time, trips.max_passengers, trips.origin_address, origin.name, origin.city_id, origin.province_id, destination_address, destination.name, destination.city_id, destination.province_id, users.email, users.user_id, users.phone, cars.car_id, cars.plate, cars.info_car\n" +
-                "ORDER BY trips.origin_date_time ASC \n" +
-                "LIMIT ?;",ROW_MAPPER,n);
+        QueryBuilder queryBuilder = new QueryBuilder()
+                .withOrderBy(QueryBuilder.DbField.ORIGIN_DATE_TIME, QueryBuilder.DbOrder.ASC)
+                .withLimit(n);
+        return jdbcTemplate.query(queryBuilder.getString(),ROW_MAPPER,queryBuilder.getArguments().toArray());
     }
     //Preguntar:
     //reutilizar query -> concatenar string
@@ -115,18 +114,125 @@ public class TripDaoImpl implements TripDao {
     //Backup BD (si no la semana que viene)
     @Override
     public List<Trip> getTripsByDateTimeAndOriginAndDestination(long origin_city_id, long destination_city_id, LocalDateTime dateTime){
-        return jdbcTemplate.query("SELECT trips.trip_id, trips.max_passengers, trips.origin_date_time, trips.origin_address, origin.name as origin_city_name, origin.city_id as origin_city_id, origin.province_id as origin_province_id, trips.destination_address, destination.name as destination_city_name, destination.city_id as destination_city_id, destination.province_id as destination_province_id, users.email as user_email, users.user_id as user_id, users.phone as user_phone, cars.car_id as car_id, cars.plate  as car_plate, cars.info_car as car_info_car, count(passengers.user_id) as occupied_seats\n" +
-                "FROM trips NATURAL JOIN trips_cars_drivers NATURAL JOIN users NATURAL JOIN cars JOIN cities origin ON trips.origin_city_id = origin.city_id JOIN cities destination ON trips.destination_city_id=destination.city_id LEFT OUTER JOIN passengers ON passengers.trip_id = trips.trip_id\n" +
-                "WHERE origin.city_id = ? AND destination.city_id =? AND trips.origin_date_time = ? "+
-                "GROUP BY trips.trip_id,trips.origin_date_time, trips.max_passengers, trips.origin_address, origin.name, origin.city_id, origin.province_id, destination_address, destination.name, destination.city_id, destination.province_id, users.email, users.user_id, users.phone, cars.car_id, cars.plate, cars.info_car\n" +
-                "ORDER BY trips.origin_date_time ASC \n" +
-                "LIMIT 10;",ROW_MAPPER,origin_city_id,destination_city_id, dateTime);
+        QueryBuilder queryBuilder = new QueryBuilder()
+                .withWhere(QueryBuilder.DbField.ORIGIN_CITY_ID, QueryBuilder.DbComparator.EQUALS,origin_city_id)
+                .withWhere(QueryBuilder.DbField.DESTINATION_CITY_ID, QueryBuilder.DbComparator.EQUALS,destination_city_id)
+                .withWhere(QueryBuilder.DbField.ORIGIN_DATE_TIME, QueryBuilder.DbComparator.EQUALS,dateTime)
+                .withOrderBy(QueryBuilder.DbField.ORIGIN_DATE_TIME, QueryBuilder.DbOrder.ASC)
+                .withLimit(10);
+        return jdbcTemplate.query(queryBuilder.getString(),ROW_MAPPER,queryBuilder.getArguments().toArray());
     }
     @Override
     public Optional<Trip> findById(long tripId) {
-        return jdbcTemplate.query("SELECT trips.trip_id, trips.max_passengers, trips.origin_date_time, trips.origin_address, origin.name as origin_city_name, origin.city_id as origin_city_id, origin.province_id as origin_province_id, trips.destination_address, destination.name as destination_city_name, destination.city_id as destination_city_id, destination.province_id as destination_province_id, users.email as user_email, users.user_id as user_id, users.phone as user_phone, cars.car_id as car_id, cars.plate  as car_plate, cars.info_car as car_info_car, count(passengers.user_id) as occupied_seats\n" +
-                "FROM trips NATURAL JOIN trips_cars_drivers NATURAL JOIN users NATURAL JOIN cars JOIN cities origin ON trips.origin_city_id = origin.city_id JOIN cities destination ON trips.destination_city_id=destination.city_id LEFT OUTER JOIN passengers ON passengers.trip_id = trips.trip_id\n" +
-                "WHERE trips.trip_id = ?\n"+
-                "GROUP BY trips.trip_id, trips.max_passengers, trips.origin_address, origin.name, origin.city_id, origin.province_id, destination_address, destination.name, destination.city_id, destination.province_id, users.email, users.user_id, users.phone, cars.car_id, cars.plate, cars.info_car;",ROW_MAPPER,tripId).stream().findFirst();
+        QueryBuilder queryBuilder = new QueryBuilder()
+                .withWhere(QueryBuilder.DbField.TRIP_ID, QueryBuilder.DbComparator.EQUALS, tripId);
+        return jdbcTemplate.query(queryBuilder.getString(),ROW_MAPPER,queryBuilder.getArguments().toArray()).stream().findFirst();
+    }
+
+
+    private static class QueryBuilder{
+        private final String select = "SELECT trips.trip_id, trips.max_passengers, trips.origin_date_time, trips.origin_address, origin.name as origin_city_name, origin.city_id as origin_city_id, origin.province_id as origin_province_id, trips.destination_address, destination.name as destination_city_name, destination.city_id as destination_city_id, destination.province_id as destination_province_id, users.email as user_email, users.user_id as user_id, users.phone as user_phone, cars.car_id as car_id, cars.plate  as car_plate, cars.info_car as car_info_car, count(passengers.user_id) as occupied_seats";
+        private final String from = "FROM trips NATURAL JOIN trips_cars_drivers NATURAL JOIN users NATURAL JOIN cars JOIN cities origin ON trips.origin_city_id = origin.city_id JOIN cities destination ON trips.destination_city_id=destination.city_id LEFT OUTER JOIN passengers ON passengers.trip_id = trips.trip_id";
+        private final StringBuilder where = new StringBuilder();
+        private final String groupBy = "GROUP BY trips.trip_id,trips.origin_date_time, trips.max_passengers, trips.origin_address, origin.name, origin.city_id, origin.province_id, destination_address, destination.name, destination.city_id, destination.province_id, users.email, users.user_id, users.phone, cars.car_id, cars.plate, cars.info_car";
+        private final StringBuilder orderBy = new StringBuilder();
+        private final StringBuilder limit = new StringBuilder();
+        List<Object> whereArguments = new ArrayList<>();
+        List<Object> limitArguments = new ArrayList<>();
+
+        public String getString(){
+            StringBuilder ans = new StringBuilder();
+            return ans.append(select).append('\n').append(from).append('\n').append(where).append('\n').append(groupBy).append('\n').append(orderBy).append('\n').append(limit).append(';').toString();
+        }
+        public List<Object> getArguments(){
+            List<Object> ans = new ArrayList<>();
+            ans.addAll(whereArguments);
+            ans.addAll(limitArguments);
+            return ans;
+        }
+        public QueryBuilder withWhere(DbField field, DbComparator comparator, Object value){
+            if(this.where.length()>0){
+                this.where.append(" AND ");
+            }else{
+                this.where.append("WHERE ");
+            }
+            this.where.append(field.dbName).append(' ').append(comparator.dbName).append(" ?");
+            this.whereArguments.add(value);
+            return this;
+        }
+        public QueryBuilder withOrderBy(DbField field, DbOrder order){
+            if(this.orderBy.length()>0){
+                this.orderBy.append(",");
+            }else{
+                this.orderBy.append(" ORDER BY ");
+            }
+            this.orderBy.append(field.dbName).append(' ').append(order.dbName).append(' ');
+            return this;
+        }
+        public QueryBuilder withLimit(Object limit){
+            if(this.limit.length()>0){
+                return this;
+            }
+            this.limit.append("LIMIT ?");
+            this.limitArguments.add(limit);
+            return this;
+        }
+
+        private enum DbField{
+            TRIP_ID("trips.trip_id"),
+            MAX_PASSENGERS("trips.max_passengers"),
+            ORIGIN_DATE_TIME("trips.origin_date_time"),
+            ORIGIN_ADDRESS("trips.origin_address"),
+            ORIGIN_NAME("origin.name"),
+            ORIGIN_CITY_ID("origin.city_id"),
+            ORIGIN_PROVINCE_ID("origin.province_id"),
+            DESTINATION_ADDRESS("trips.destination_address"),
+            DESTINATION_NAME("destination.name"),
+            DESTINATION_CITY_ID("destination.city_id"),
+            DESTINATION_PROVINCE_ID("destination.province_id"),
+            USER_EMAIL("users.email"),
+            USER_ID("users.user_id"),
+            USER_PHONE("users.phone"),
+            CAR_ID("cars.car_id"),
+            CAR_PLATE("cars.plate"),
+            CAR_INFO_CAR("cars.info_car"),
+            OCCUPIED_SEATS(" count(passengers.user_id)");
+
+            private final String dbName;
+            private DbField(String dbName){
+                this.dbName = dbName;
+            }
+
+            public String getDbName() {
+                return dbName;
+            }
+        }
+        private enum DbComparator{
+            EQUALS("="),
+            GREATER(">"),
+            LESS("<"),
+            GREATER_OR_EQUALS(">="),
+            LESS_OR_EQUAL("<=");
+            private final String dbName;
+            private DbComparator(String dbName){
+                this.dbName = dbName;
+            }
+
+            public String getDbName() {
+                return dbName;
+            }
+        }
+        private enum DbOrder{
+            ASC("ASC"),
+            DESC("DESC");
+            private final String dbName;
+            private DbOrder(String dbName){
+                this.dbName = dbName;
+            }
+
+            public String getDbName() {
+                return dbName;
+            }
+        }
     }
 }
