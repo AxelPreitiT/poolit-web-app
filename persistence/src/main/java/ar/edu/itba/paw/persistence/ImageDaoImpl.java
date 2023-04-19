dd088af39ab1a6b09c4034256c643e588f6c6e4f
nicosuarezdurrels <nicosuarez272@gmail.com>
2023-04-19 12:01:17 -0300

    feat: Add Image services and persistances implementations

---
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/ImageDaoImpl.java
@@ -0,0 +1,52 @@
+package ar.edu.itba.paw.persistence;
+
+
+import ar.edu.itba.paw.interfaces.persistence.ImageDao;
+import ar.edu.itba.paw.models.Image;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class ImageDaoImpl implements ImageDao {
+
+    private static final RowMapper<Image> ROW_MAPPER = (resultSet, rowNum) -> new Image(resultSet.getLong("image_id"),resultSet.getBytes("data"));
+
+    private final JdbcTemplate jdbcTemplate;
+
+    private final SimpleJdbcInsert jdbcInsert;
+
+    @Autowired
+    public ImageDaoImpl(final DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.jdbcInsert = new SimpleJdbcInsert(dataSource)
+                .usingGeneratedKeyColumns("id")
+                .withTableName("images");
+    }
+
+    @Override
+    public Image create(byte[] data) {
+        Map<String,Object> parameters = new HashMap<>();
+        parameters.put("data", data);
+        Number key = jdbcInsert.executeAndReturnKey(parameters);
+        return new Image(key.longValue(), data);
+    }
+
+
+    @Override
+    public Optional<Image> findById(long imageId){
+        return jdbcTemplate.query("SELECT * FROM images WHERE image_id = ?",ROW_MAPPER,imageId).stream().findFirst();
+    }
+
+}
+
+
+
+
