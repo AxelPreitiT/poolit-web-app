db38255426ac98349fa581b2f28a5add64b97ba0
JoseMenta <jmentasti@itba.edu.ar>
2023-04-30 16:29:51 -0300

    feat: enable async mails

--- services/src/main/java/ar/edu/itba/paw/services/EmailServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/EmailServiceImpl.java
@@ -4,11 +4,13 @@ import ar.edu.itba.paw.interfaces.services.EmailService;
 import ar.edu.itba.paw.models.trips.Trip;
 import ar.edu.itba.paw.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.util.StreamUtils;
 import org.springframework.stereotype.Service;
-import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 
@@ -23,6 +25,21 @@ import java.util.Map;
 @Service
 public class EmailServiceImpl implements EmailService {
 
+    private static final String FROM = "poolit.noreply@gmail.com";
+
+    private final SpringTemplateEngine templateEngine;
+
+    private final JavaMailSender mailSender;
+
+    private final MessageSource messageSource;
+    @Autowired
+    public EmailServiceImpl(SpringTemplateEngine templateEngine, JavaMailSender mailSender, MessageSource messageSource){
+        this.templateEngine = templateEngine;
+        templateEngine.setTemplateEngineMessageSource(messageSource);
+        this.mailSender = mailSender;
+        this.messageSource = messageSource;
+    }
+
     private Map<String, Object> getVariablesMap(Context context) {
         Map<String, Object> variablesMap = new HashMap<>();
         for (String variableName : context.getVariableNames()) {
@@ -31,114 +48,84 @@ public class EmailServiceImpl implements EmailService {
         return variablesMap;
     }
 
-    @Autowired
-    private SpringTemplateEngine templateEngine;
-
-    @Autowired
-    private JavaMailSender mailSender;
-
-    @Override
-    public void sendMailNewPassenger(Trip trip, User passenger) throws MessagingException, IOException {
-        MimeMessage message = mailSender.createMimeMessage();
-        message.setFrom(new InternetAddress("poolit.noreply@gmail.com"));
-        message.addRecipient(Message.RecipientType.TO,new InternetAddress(trip.getDriver().getEmail()));
-        message.setSubject("Nuevo pasajero en tu viaje!");
-        // Variables para el html
-        final Context ctx = new Context();
-        ctx.setVariable("trip", trip);
-        ctx.setVariable("passenger", passenger);
-
-        // Cargo el template
-        final String htmlContent = loadTemplate("new-passenger-mail", getVariablesMap(ctx));
-
-        message.setContent(htmlContent,"text/html; charset=UTF-8");
-
-        mailSender.send(message);
-    }
-    //TODO: catch exceptions here and send others
-    @Override
-    public void sendMailTripDeletedToPassenger(Trip trip, User passenger) throws MessagingException,IOException{
-        MimeMessage message = mailSender.createMimeMessage();
-        message.setFrom(new InternetAddress("poolit.noreply@gmail.com"));
-        message.addRecipient(Message.RecipientType.TO,new InternetAddress(passenger.getEmail()));
-        message.setSubject("Tu viaje fue cancelado");
-        // Variables para el html
-        final Context ctx = new Context();
-        ctx.setVariable("trip", trip);
-        ctx.setVariable("passenger", passenger);
-
-        // Cargo el template
-        final String htmlContent = loadTemplate("delete-trip-passenger-mail", getVariablesMap(ctx));
-
-        message.setContent(htmlContent,"text/html; charset=UTF-8");
-
-        mailSender.send(message);
-    }
-    @Override
-    public void sendMailTripDeletedToDriver(Trip trip) throws MessagingException,IOException{
-        MimeMessage message = mailSender.createMimeMessage();
-        message.setFrom(new InternetAddress("poolit.noreply@gmail.com"));
-        message.addRecipient(Message.RecipientType.TO,new InternetAddress(trip.getDriver().getEmail()));
-        message.setSubject("Nuevo pasajero en tu viaje!");
-        // Variables para el html
-        final Context ctx = new Context();
-        ctx.setVariable("trip", trip);
-
-        // Cargo el template
-        final String htmlContent = loadTemplate("delete-trip-driver-mail", getVariablesMap(ctx));
-
-        message.setContent(htmlContent,"text/html; charset=UTF-8");
-
-        mailSender.send(message);
-    }
-
-
-
-    @Override
-    public void sendMailNewTrip(Trip trip) throws MessagingException, IOException {
-        MimeMessage message = mailSender.createMimeMessage();
-        message.setFrom(new InternetAddress("poolit.noreply@gmail.com"));
-        message.addRecipient(Message.RecipientType.TO,new InternetAddress(trip.getDriver().getEmail()));
-        message.setSubject("Nuevo viaje en Poolit creado!");
-
-        final Context ctx = new Context();
-        ctx.setVariable("trip", trip);
-
-        final String htmlContent = loadTemplate("create-trip-mail", getVariablesMap(ctx));
-
-        message.setContent(htmlContent, "text/html; charset=UTF-8");
-
-        mailSender.send(message);
-    }
-
-    @Override
-    public void sendMailTripConfirmation(Trip trip, User passenger) throws MessagingException, IOException {
-        MimeMessage message = mailSender.createMimeMessage();
-        message.setFrom(new InternetAddress("poolit.noreply@gmail.com"));
-
-        message.addRecipient(Message.RecipientType.TO,new InternetAddress(passenger.getEmail()));
-        message.setSubject("Nuevo viaje confirmado!");
-
-        final Context ctx = new Context();
-        ctx.setVariable("trip", trip);
-        ctx.setVariable("passenger", passenger);
-
-        final String htmlContent = loadTemplate("trip-confirmation-mail", getVariablesMap(ctx));
-
-        message.setContent(htmlContent, "text/html; charset=UTF-8");
-
-        mailSender.send(message);
-    }
-
     private String loadTemplate(String name, Map<String, Object> model) throws IOException {
         final String templateFileName = name + ".html";
         final ClassPathResource resource = new ClassPathResource("templates/" + templateFileName);
         final byte[] templateBytes = StreamUtils.copyToByteArray(resource.getInputStream());
         final String template = new String(templateBytes, StandardCharsets.UTF_8);
-        final TemplateEngine templateEngine = new SpringTemplateEngine();
-        final Context context = new Context();
+        final Context context = new Context(LocaleContextHolder.getLocale());
         context.setVariables(model);
         return templateEngine.process(template, context);
+    }
+
+    @Async
+    public void sendEmail(final String to,final String subject,final String emailTemplate,final Context context) throws MessagingException, IOException {
+        MimeMessage message = mailSender.createMimeMessage();
+
+        message.setFrom(new InternetAddress(FROM));
+
+        message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));
+
+        message.setSubject(subject,"UTF-8");
+
+        final String htmlContent = loadTemplate(emailTemplate, getVariablesMap(context));
+
+        message.setContent(htmlContent,"text/html; charset=UTF-8");
+
+        mailSender.send(message);
+    }
+
+    @Override
+    public void sendMailNewPassenger(Trip trip, User passenger) throws MessagingException, IOException {
+        String subject = messageSource.getMessage("emails.subject.newPassengerDriver",null,LocaleContextHolder.getLocale());
+        // Variables para el html
+        final Context ctx = new Context();
+        ctx.setVariable("trip", trip);
+        ctx.setVariable("passenger", passenger);
+        //enviamos el mail
+        sendEmail(trip.getDriver().getEmail(),subject,"new-passenger-mail",ctx);
+    }
+
+    @Override
+    public void sendMailTripDeletedToPassenger(Trip trip, User passenger) throws MessagingException,IOException{
+        String subject = messageSource.getMessage("emails.subject.tripCancelledPassenger",null,LocaleContextHolder.getLocale());
+
+        final Context ctx = new Context();
+        ctx.setVariable("trip", trip);
+        ctx.setVariable("passenger", passenger);
+
+        sendEmail(passenger.getEmail(),subject,"delete-trip-passenger-email",ctx);
+    }
+
+    @Override
+    public void sendMailTripDeletedToDriver(Trip trip) throws MessagingException,IOException{
+        String subject = messageSource.getMessage("emails.subject.tripCancelledDriver",null,LocaleContextHolder.getLocale());
+        // Variables para el html
+        final Context ctx = new Context();
+        ctx.setVariable("trip", trip);
+
+        sendEmail(trip.getDriver().getEmail(),subject,"delete-trip-driver-mail",ctx);
+    }
+
+    @Override
+    public void sendMailNewTrip(Trip trip) throws MessagingException, IOException {
+        String subject = messageSource.getMessage("emails.subject.newTripCreated",null,LocaleContextHolder.getLocale());
+
+        final Context ctx = new Context();
+        ctx.setVariable("trip", trip);
+
+        sendEmail(trip.getDriver().getEmail(),subject,"create-trip-mail",ctx);
+    }
+
+    @Override
+    public void sendMailTripConfirmation(Trip trip, User passenger) throws MessagingException, IOException {
+        String subject = messageSource.getMessage("emails.subject.newTripPassenger",null,LocaleContextHolder.getLocale());
+
+        final Context ctx = new Context();
+        ctx.setVariable("trip", trip);
+        ctx.setVariable("passenger", passenger);
+
+        sendEmail(passenger.getEmail(),subject,"trip-confirmation-mail",ctx);
     }
 
 }
--- services/src/main/java/ar/edu/itba/paw/services/config/EmailConfig.java
+++ services/src/main/java/ar/edu/itba/paw/services/config/EmailConfig.java
@@ -4,30 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.util.Properties;
 
 @Configuration
+@EnableAsync
 public class EmailConfig {
-
-//    @Value("${spring.mail.host}")
-//    private String host;
-//
-//    @Value("${spring.mail.port}")
-//    private String port;
-//
-//    @Value("${spring.mail.username}")
-//    private String username;
-//
-//    @Value("${spring.mail.password}")
-//    private String password;
-//
-//    @Value("${spring.mail.properties.mail.smtp.auth}")
-//    private String smtpAuth;
-//
-//    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
-//    private String starttlsEnable;
-
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
