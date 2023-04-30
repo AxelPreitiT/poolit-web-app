feed60b9af87616771535d9a351d68d91466f196
nicosuarezdurrels <nicosuarez272@gmail.com>
2023-04-30 20:57:46 -0300

    feature: CarForm and MultiPartFile annotation

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/annotations/MPFile.java
@@ -0,0 +1,26 @@
+package ar.edu.itba.paw.webapp.form.annotations;
+
+
+import ar.edu.itba.paw.webapp.form.constraints.MultiPartFileValidator;
+import ar.edu.itba.paw.webapp.form.constraints.PasswordMatchesValidatorConstraint;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({TYPE, FIELD, ANNOTATION_TYPE})
+@Retention(RUNTIME)
+@Constraint(validatedBy = MultiPartFileValidator.class)
+@Documented
+public @interface MPFile {
+    String message() default "Imagen invalida";
+
+    Class<?> [] groups() default {};
+
+    Class<? extends Payload> [] payload() default {};
+}
