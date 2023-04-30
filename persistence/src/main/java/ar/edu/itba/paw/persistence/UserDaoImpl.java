d5fa740e9d670862d66d51f3127836dcf2a338b8
Gaston Francois <gfrancois@itba.edu.ar>
2023-04-30 19:08:31 -0300

    feat: actualice prev user data

--- persistence/src/main/java/ar/edu/itba/paw/persistence/UserDaoImpl.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/UserDaoImpl.java
@@ -10,7 +10,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
-
+import ar.edu.itba.paw.interfaces.exceptions.EmailAlreadyExistsException;
 import javax.sql.DataSource;
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -21,11 +21,11 @@ import java.util.Optional;
 public class UserDaoImpl implements UserDao {
 
     protected static final RowMapper<User> ROW_MAPPER = (resultSet, rowNum) ->
-            new User(resultSet.getLong("user_id"),resultSet.getString("username"),
-                    resultSet.getString("surname"),resultSet.getString("email"),
-                    resultSet.getString("phone"),resultSet.getString("password"),
+            new User(resultSet.getLong("user_id"), resultSet.getString("username"),
+                    resultSet.getString("surname"), resultSet.getString("email"),
+                    resultSet.getString("phone"), resultSet.getString("password"),
                     resultSet.getTimestamp("birthdate").toLocalDateTime(),
-                    new City(resultSet.getLong("city_id"),resultSet.getString("name"), resultSet.getLong("province_id")),
+                    new City(resultSet.getLong("city_id"), resultSet.getString("name"), resultSet.getLong("province_id")),
                     resultSet.getString("user_role"));
 
     private final JdbcTemplate jdbcTemplate;
@@ -33,44 +33,60 @@ public class UserDaoImpl implements UserDao {
     private final SimpleJdbcInsert jdbcInsert;
 
     @Autowired
-    public UserDaoImpl(final DataSource dataSource){
+    public UserDaoImpl(final DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
         this.jdbcInsert = new SimpleJdbcInsert(dataSource)
                 .usingGeneratedKeyColumns("user_id")
                 .withTableName("users");
     }
+
     @Override
     public User create(final String username, final String surname, final String email,
                        final String phone, final String password, final LocalDateTime birthdate, final City bornCity, String role) {
-        Map<String,Object> data = new HashMap<>();
-        String savedEmail = email.toLowerCase().replaceAll("\\s","");
-        String savedPhone = phone.replaceAll("\\s","");
-        String savedPassword = password.replaceAll("\\s","");
-        data.put("username",username);
-        data.put("surname",surname);
-        data.put("email",savedEmail);
-        data.put("phone",savedPhone);
-        data.put("password",savedPassword);
-        data.put("birthdate",birthdate);
-        data.put("city_id",bornCity.getId());
-        data.put("user_role",role);
-        Number key = jdbcInsert.executeAndReturnKey(data);
-        return new User(key.longValue(),username,surname, savedEmail, savedPhone, savedPassword, birthdate, bornCity, role);
+        Map<String, Object> data = new HashMap<>();
+        String savedEmail = email.toLowerCase().replaceAll("\\s", "");
+        String savedPhone = phone.replaceAll("\\s", "");
+        String savedPassword = password.replaceAll("\\s", "");
+        data.put("username", username);
+        data.put("surname", surname);
+        data.put("email", savedEmail);
+        data.put("phone", savedPhone);
+        data.put("password", savedPassword);
+        data.put("birthdate", birthdate);
+        data.put("city_id", bornCity.getId());
+        data.put("user_role", role);
+        Number key = 0;
+        try {
+            key = jdbcInsert.executeAndReturnKey(data);
+        } catch (DuplicateKeyException e) {
+            if (e.getMessage().contains("users_email_key")) {
+                if (findByEmail(email).get().getPassword() != null) {
+                    throw new EmailAlreadyExistsException();
+                } else {
+                    updateProfile(email, password);
+                }
+            }
+        }
+        return new User(key.longValue(), username, surname, savedEmail, savedPhone, savedPassword, birthdate, bornCity, role);
     }
 
     @Override
-    public Optional<User> findById(long userId){
-        return jdbcTemplate.query("SELECT * FROM users NATURAL JOIN cities WHERE user_id = ?",ROW_MAPPER,userId).stream().findFirst();
+    public Optional<User> findById(long userId) {
+        return jdbcTemplate.query("SELECT * FROM users NATURAL JOIN cities WHERE user_id = ?", ROW_MAPPER, userId).stream().findFirst();
     }
 
     @Override
-    public Optional<User> findByEmail(String email){
-        String searchEmail = email.toLowerCase().replaceAll("\\s","");
-        return jdbcTemplate.query("SELECT * FROM users NATURAL JOIN cities WHERE email = ?",ROW_MAPPER,searchEmail).stream().findFirst();
+    public Optional<User> findByEmail(String email) {
+        String searchEmail = email.toLowerCase().replaceAll("\\s", "");
+        return jdbcTemplate.query("SELECT * FROM users NATURAL JOIN cities WHERE email = ?", ROW_MAPPER, searchEmail).stream().findFirst();
     }
 
     @Override
     public void changeRole(long userId, String role) {
-        jdbcTemplate.update("UPDATE users SET user_role = ? WHERE user_id = ?", role ,userId);
+        jdbcTemplate.update("UPDATE users SET user_role = ? WHERE user_id = ?", role, userId);
+    }
+
+    public void updateProfile(final String email, final String password){
+        jdbcTemplate.update("UPDATE users SET password = ?  WHERE email = ?", password, email);
     }
 }
