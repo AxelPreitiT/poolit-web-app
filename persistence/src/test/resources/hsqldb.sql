249e7e3fa36d88105bab393c9f81964b7d2bc2aa
JoseMenta <jmentasti@itba.edu.ar>
2023-05-14 13:20:25 -0300

    test: add tests for ReviewDaoImpl

--- persistence/src/test/resources/hsqldb.sql
+++ persistence/src/test/resources/hsqldb.sql
@@ -79,3 +79,13 @@ CREATE TABLE IF NOT EXISTS trips_cars_drivers(
                                                  CONSTRAINT trips_to_users FOREIGN KEY(user_id) REFERENCES users(user_id),
                                                  CONSTRAINT trips_to_cars FOREIGN KEY(car_id) references cars(car_id)
 );
+CREATE TABLE IF NOT EXISTS reviews(
+                                      review_id IDENTITY PRIMARY KEY,
+                                      trip_id INT NOT NULL,
+                                      user_id INT NOT NULL,
+                                      rating INT NOT NULL,
+                                      review VARCHAR(2000) NOT NULL,
+                                      UNIQUE(trip_id,user_id),
+                                      CONSTRAINT reviews_to_trips FOREIGN KEY(trip_id) REFERENCES trips(trip_id),
+                                      CONSTRAINT reviews_to_users FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
