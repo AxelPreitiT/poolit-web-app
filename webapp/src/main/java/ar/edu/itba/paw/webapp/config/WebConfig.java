5df69181a2c936b254f9f187fb7975536259d3f1
JoseMenta <jmentasti@itba.edu.ar>
2023-05-16 19:56:30 -0300

    chore: change properties for deploy

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -57,23 +57,12 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         return viewResolver;
     }
 
-//    @Bean
-//    public DataSource dataSource() {
-//        LOGGER.info("Connecting to Postgres DB");
-//        final SimpleDriverDataSource ds = new SimpleDriverDataSource();
-//        ds.setDriverClass(org.postgresql.Driver.class);
-//        ds.setUrl(String.format("jdbc:postgresql://localhost/%s",environment.getProperty("DB_NAME")));
-//        ds.setUsername(environment.getProperty("DB_USER"));
-//        ds.setPassword(environment.getProperty("DB_PASSWORD"));
-//        return ds;
-//    }
-
     @Bean
     public DataSource dataSource() {
         LOGGER.info("Connecting to Postgres DB");
         final SimpleDriverDataSource ds = new SimpleDriverDataSource();
         ds.setDriverClass(org.postgresql.Driver.class);
-        ds.setUrl(String.format("jdbc:postgresql://localhost:%s/%s",environment.getProperty("DB_PORT"),environment.getProperty("DB_NAME")));
+        ds.setUrl(String.format("jdbc:postgresql://localhost/%s",environment.getProperty("DB_NAME")));
         ds.setUsername(environment.getProperty("DB_USER"));
         ds.setPassword(environment.getProperty("DB_PASSWORD"));
         return ds;
