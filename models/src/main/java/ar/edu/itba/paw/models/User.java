2d6646e6cd4aaae83503809de0558dfa5166bec4
JoseMenta <jmentasti@itba.edu.ar>
2023-05-22 21:44:39 -0300

    chore: start migration of User model (wait for relations for City)

--- models/src/main/java/ar/edu/itba/paw/models/User.java
+++ models/src/main/java/ar/edu/itba/paw/models/User.java
@@ -1,19 +1,41 @@
 package ar.edu.itba.paw.models;
 
+import javax.persistence.*;
 import java.util.Locale;
 import java.util.Objects;
 
+@Entity
 public class User {
-    private final long userId;
-    private final String name;
-    private final String surname;
-    private final String email;
+    @Id
+    @GeneratedValue(strategy =  GenerationType.SEQUENCE, generator ="users_user_id_seq" )
+    @SequenceGenerator(sequenceName = "users_user_id_seq" , name = "users_user_id_seq", allocationSize = 1)
+    @Column(name = "user_id")
+    private long userId;
+    @Column(name = "username")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "phone")
     private String phone;
-    private final String password;
-    private final City bornCity;
-    private final Locale mailLocale;
+    @Column(name = "password")
+    private String password;
+
+    //TODO: mapear relaciones
+    private City bornCity;
+    @Column(name = "mail_locale")
+    //Si no funciona, ir aca
+    //https://www.baeldung.com/jpa-attribute-converters
+    private Locale mailLocale;
+    @Column(name = "user_role")
     private String role;
+    @Column(name = "user_image_id")
     private long userImageId;
+
+    protected User(){
+
+    }
 
     public User(long userId, final String name, final String surname, final String email,
                 final String phone, String password, final City bornCity, final Locale mailLocale, final String role,long userImageId) {
