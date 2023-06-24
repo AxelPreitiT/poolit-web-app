fe524aeed13f2910e14338dddc0cef085b5bdf10
Axel Facundo Preiti Tasat <apreititasat@itba.edu.ar>
2023-06-24 12:10:46 -0300

    feat: Show only accepted trips in reserved history page

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/persistence/TripDao.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/persistence/TripDao.java
@@ -25,7 +25,7 @@ public interface TripDao {
     PagedContent<TripInstance> getTripInstances(final Trip trip, int page, int pageSize, LocalDateTime start, LocalDateTime end);
     int getTripSeatCount(long tripId, LocalDateTime startDateTime, LocalDateTime endDateTime);
     PagedContent<Trip> getTripsCreatedByUser(final User user,Optional<LocalDateTime> minDateTime, Optional<LocalDateTime> maxDateTime,int page, int pageSize);
-    PagedContent<Trip> getTripsWhereUserIsPassenger(final User user,Optional<LocalDateTime> minDateTime, Optional<LocalDateTime> maxDateTime, int page, int pageSize);
+    PagedContent<Trip> getTripsWhereUserIsPassenger(final User user, Optional<LocalDateTime> minDateTime, Optional<LocalDateTime> maxDateTime, Passenger.PassengerState passengerState, int page, int pageSize);
     Optional<Trip> findById(long id);
     Optional<Trip> findById(long tripId, LocalDateTime start, LocalDateTime end);
     boolean deleteTrip(final Trip trip);
