c13ca89f0ed790f8af0ead36318cca115dde82c2
Axel Facundo Preiti Tasat <apreititasat@itba.edu.ar>
2023-10-14 14:10:02 -0300

    [FRONT] chore: Add router and i18n

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./globals.scss";
+import "./i18n.ts";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
---
+++ frontend/src/pages/login/LoginPage.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import styles from "./styles.module.scss";
+import { Link } from "react-router-dom";
+import PoolitLogo from "@/assets/poolit.svg";
+import { useTranslation } from "react-i18next";
+
+const LoginPage = () => {
+  const { t, i18n } = useTranslation();
+  console.log(i18n.languages);
+
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.logoContainer}>
+        <Link to="/">
+          <img src={PoolitLogo} alt={t("poolit.logo")} />
+        </Link>
+      </div>
+      <div className={styles.loginContainer}>
+        <form className={styles.formContainer}>
+          <div>
+            <h3 className="light-text fw-bold">{t("login.title")}</h3>
+            <hr className="light-text" />
+          </div>
+          <div className={styles.inputContainer}>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="form-control"
+              placeholder={t("login.email")}
+            />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="form-control"
+              placeholder={t("login.password")}
+            />
+            <div className={styles.rememberContainer}>
+              <input
+                type="checkbox"
+                id="remember"
+                name="remember"
+                className="form-check-input mt-0"
+              />
+              <label htmlFor="remember" className="light-text">
+                {t("login.remember")}
+              </label>
+            </div>
+          </div>
+          <button type="submit" className="btn secondary-btn">
+            <h5>{t("login.login")}</h5>
+          </button>
+        </form>
+        <div className={styles.loginFooterContainer}>
+          <hr className="light-text" />
+          <h4 className="light-text">{t("register.not_registered")}</h4>
+          <Link to="/register">
+            <h5 className="link-text fw-bold">{t("register.register")}</h5>
+          </Link>
+        </div>
+      </div>
+      <div className={styles.footerContainer}>
+        <h6>{t("verify_email.request")}</h6>
+        <Link to="/verify-email">
+          <h6 className="link-text fw-bold">{t("verify_email.here")}</h6>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
