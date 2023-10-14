0befe7523da0bc5b49c6744bb2bf82f106bab669
Axel Facundo Preiti Tasat <apreititasat@itba.edu.ar>
2023-10-14 12:19:17 -0300

    [FRONT] chore: Create vite.js app
    - Remove next.js files and configs

--- frontend/poolit_nextjs/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  trailingSlash: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH
-};
-
-module.exports = nextConfig;
