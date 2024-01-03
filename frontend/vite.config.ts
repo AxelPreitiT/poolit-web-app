5b0eefb2e79e5be9a6a55befe0a8fc703b103c18
Axel Facundo Preiti Tasat <apreititasat@itba.edu.ar>
2024-01-03 19:01:30 -0300

    [FRONT] chore: Load app base path from env file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [react(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      "@": "/src",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react(), tsconfigPaths()],
+    resolve: {
+      alias: {
+        "@": "/src",
+      },
     },
-  },
-  base: mode === "production" ? "/paw-2023a-07" : "/",
-}));
+    base: env.VITE_BASE_PATH,
+  };
+});
