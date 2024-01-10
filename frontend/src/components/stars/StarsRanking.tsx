5635514233e2256703e99ab58825da8f21418dd6
Gaston Francois <gfrancois@itba.edu.ar>
2024-01-10 03:00:36 -0300

    [FRONT] style: add style in trip info

--- frontend/src/components/stars/StarsRanking.tsx
+++ frontend/src/components/stars/StarsRanking.tsx
@@ -1,19 +1,31 @@
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "./styles.module.scss";
 
 interface StarRatingProps {
   rating: number;
+  color?: string;
+  size?: string;
 }
 
-const StarRating = ({ rating }: StarRatingProps) => {
+const StarRating = ({
+  rating,
+  color = "#ffa216",
+  size = "h4",
+}: StarRatingProps) => {
   const renderStar = (index: number) => {
     const remainder = rating - index + 0.5;
+    const starStyle = {
+      color,
+      fontSize: size,
+    };
+
     if (remainder >= 1) {
-      return <i className="bi bi-star-fill text-warning h4" key={index}></i>;
+      return <i className="bi bi-star-fill" style={starStyle} key={index}></i>;
     } else if (remainder > 0) {
-      return <i className="bi bi-star-half text-warning h4" key={index}></i>;
+      return <i className="bi bi-star-half" style={starStyle} key={index}></i>;
     } else {
-      return <i className="bi bi-star text-warning h4" key={index}></i>;
+      return <i className="bi bi-star" style={starStyle} key={index}></i>;
     }
   };
 
--- frontend/src/pages/trips/TripDetailsPage.tsx
+++ frontend/src/pages/trips/TripDetailsPage.tsx
@@ -1,10 +1,12 @@
+import styles from "./styles.module.scss";
 import { useParams } from "react-router-dom";
 import MainComponent from "@/components/utils/MainComponent";
 import MainHeader from "@/components/utils/MainHeader";
 import { useTranslation } from "react-i18next";
 import StatusTrip from "@/components/statusTrip/ProfileProp";
-import MovingCar from "@/components/location/MovingCar";
 import Location from "@/components/location/Location";
+import CircleImg from "@/components/img/circleImg/CircleImg";
+import StarRating from "@/components/stars/StarsRanking";
 
 const TripDetailsPage = () => {
   const { t } = useTranslation();
@@ -23,6 +25,70 @@ const TripDetailsPage = () => {
           end_address="hola"
           end_city="hola"
         />
+        <div className={styles.middle_content}>
+          <div className={styles.info_trip}>
+            <div className={styles.show_row}>
+              <i className="bi bi-clock light-text"></i>
+              <div className={styles.info_details}>
+                <span className="light-text detail">16:00</span>
+              </div>
+            </div>
+            <div className={styles.show_row}>
+              <i className="bi bi-calendar light-text"></i>
+              <div className={styles.info_details}>
+                <span className="light-text detail">FRIDAY</span>
+                <span className={styles.subtitle_info}>10/9/2014</span>
+              </div>
+            </div>
+            <div className={styles.show_row}>
+              <CircleImg
+                src="http://pawserver.it.itba.edu.ar/paw-2023a-07/image/80"
+                size={40}
+              />
+              <div className={styles.info_details}>
+                <span className="light-text detail">FRIDAY</span>
+                <StarRating rating={3.5} color={"#ffffff"} size="10" />
+              </div>
+            </div>
+            <div className={styles.show_row}>
+              <i className="bi bi-people light-text"></i>
+              <div className={styles.info_details}>
+                <span className="light-text detail">16:00</span>
+              </div>
+            </div>
+            <hr className="white" />
+            <div className={styles.show_row}>
+              <CircleImg
+                src="http://pawserver.it.itba.edu.ar/paw-2023a-07/image/80"
+                size={40}
+              />
+              <div className={styles.info_details}>
+                <span className="light-text detail">FRIDAY</span>
+                <StarRating rating={3.5} color={"#ffffff"} size="10" />
+              </div>
+            </div>
+            <div className={styles.show_row}>
+              <i className="bi bi-envelope-fill light-text"></i>
+              <div className={styles.info_details}>
+                <span className="light-text detail">16:00</span>
+              </div>
+            </div>
+            <div className={styles.show_row}>
+              <i className="bi bi-telephone-fill light-text"></i>
+              <div className={styles.info_details}>
+                <span className="light-text detail">16:00</span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.img_container}>
+            <img
+              src="http://pawserver.it.itba.edu.ar/paw-2023a-07/image/80"
+              className={styles.img_style}
+              alt=""
+            />
+          </div>
+        </div>
+
         <h1>Trip {tripId}</h1>
       </MainComponent>
       <MainComponent>
