f6ee3da87c398341a1469baa59d3a9907998f7d7
Axel Facundo Preiti Tasat <apreititasat@itba.edu.ar>
2024-01-12 00:16:33 -0300

    [FRONT] chore: Replace fixed cities endpoint with base endpoint (more RESTy)

--- frontend/src/api/CitiesApi.ts
+++ frontend/src/api/CitiesApi.ts
@@ -1,20 +1,12 @@
 import CityModel from "@/models/CityModel";
-import { AxiosPromise, AxiosRequestConfig } from "axios";
+import { AxiosPromise } from "axios";
 import AxiosApi from "./axios/AxiosApi";
 
 class CitiesApi extends AxiosApi {
-  private static readonly CITIES_BASE_PATH: string = "/cities";
-
   public static getCityById: (uri: string) => AxiosPromise<CityModel> = (
     uri: string
   ) => {
     return this.get<CityModel>(uri);
-  };
-
-  public static getOptions: (options: AxiosRequestConfig) => AxiosPromise = (
-    options: AxiosRequestConfig
-  ) => {
-    return this.options(CitiesApi.CITIES_BASE_PATH, options);
   };
 }
 
--- frontend/src/api/UtilsApi.ts
+++ frontend/src/api/UtilsApi.ts
@@ -1,12 +1,11 @@
 import { AxiosPromise, AxiosRequestConfig } from "axios";
-import CitiesApi from "./CitiesApi";
 import AxiosApi from "./axios/AxiosApi";
 
 class UtilsApi extends AxiosApi {
   public static tryAuthentication: (
     options?: AxiosRequestConfig
   ) => AxiosPromise = (options?: AxiosRequestConfig) => {
-    return CitiesApi.getOptions(options || {});
+    return AxiosApi.options("/", options || {});
   };
 }
 
