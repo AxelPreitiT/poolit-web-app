4de3b94fbbf36457c20a63db7ebef6bbd9d65b9c
Gaston Francois <gfrancois@itba.edu.ar>
2024-01-13 20:50:33 -0300

    [FRONT] chore: conect info profile

--- frontend/src/pages/profile/ProfilePage.tsx
+++ frontend/src/pages/profile/ProfilePage.tsx
@@ -7,9 +7,8 @@ import ProfileStars from "@/components/profile/stars/ProfileStars";
 import DriverList from "@/components/profile/ProfileLists/DriverList";
 import PassengerList from "@/components/profile/ProfileLists/PassangerList";
 import TabComponent from "@/components/tab/TabComponent";
-import {useEffect, useState} from "react";
-import CityModel from "@/models/CityModel.ts";
-import CityService from "@/services/CityService.ts";
+import {useCurrentUser} from "@/hooks/users/useCurrentUser.tsx";
+import UserPrivateModel from "@/models/UserPrivateModel.ts";
 
 const ProfilePage = () => {
   const { t } = useTranslation();
@@ -25,6 +24,7 @@ const ProfilePage = () => {
     rating_passenger: 1.5,
   };
 
+  /*
   const [city, setCity] = useState<CityModel|null >(null);
 
 
@@ -36,37 +36,63 @@ const ProfilePage = () => {
       })
     })
 
+   */
+
+    const {isLoading, currentUser} = useCurrentUser();
+
+    const ProfileInfo =({ currentUser }: { currentUser: UserPrivateModel }) => (
+        <div className={styles.profileCard}>
+            <ProfileImg src={ProfilePhoto} />
+            <h3 className="text-center">
+                {t("format.name", {
+                    name: currentUser.username,
+                    surname: currentUser.surname,
+                })}</h3>
+            <ProfileProp prop={t("profile.props.email")} text={currentUser.email} />
+            <ProfileProp prop={t("profile.props.phone")} text={currentUser.phone} />
+            <ProfileProp
+                prop={t("profile.props.neighborhood")}
+                text={user.neighborhood}
+            />
+            <ProfileProp prop={t("profile.props.language")} text={currentUser.mailLocale} />
+            <ProfileProp prop={t("profile.props.trips")} text={user.trips} />
+            <ProfileStars
+                prop={t("profile.props.rating_driver")}
+                rating={currentUser.driverRating}
+            />
+            <ProfileStars
+                prop={t("profile.props.rating_passenger")}
+                rating={currentUser.passengerRating}
+            />
+        </div>
+    )
 
   return (
     <div className={styles.main_container}>
-      <div className={styles.profileCard}>
-        <ProfileImg src={ProfilePhoto} />
-        <h3 className="text-center">{city?.name}</h3>
-        <ProfileProp prop={t("profile.props.email")} text={user.email} />
-        <ProfileProp prop={t("profile.props.phone")} text={user.phone} />
-        <ProfileProp
-          prop={t("profile.props.neighborhood")}
-          text={user.neighborhood}
-        />
-        <ProfileProp prop={t("profile.props.language")} text={user.language} />
-        <ProfileProp prop={t("profile.props.trips")} text={user.trips} />
-        <ProfileStars
-          prop={t("profile.props.rating_driver")}
-          rating={user.rating_driver}
-        />
-        <ProfileStars
-          prop={t("profile.props.rating_passenger")}
-          rating={user.rating_passenger}
-        />
-      </div>
+        {isLoading || currentUser===undefined ?
+            <div className={styles.profileCard}>
+                <h3>Loading...</h3>
+            </div>:
+            <ProfileInfo currentUser={currentUser} />
+        }
 
       <div className={styles.list_block}>
-        <TabComponent
-          right_title={t("roles.passenger")}
-          right_component={<PassengerList />}
-          left_title={t("roles.driver")}
-          left_component={<DriverList />}
-        />
+          {isLoading || currentUser===undefined ?
+              <TabComponent
+                  right_title={t("roles.passenger")}
+                  right_component={<h3>Loading...</h3>}
+                  left_title={t("roles.driver")}
+                  left_component={<h3>Loading...</h3>}
+              />:
+              <TabComponent
+                  right_title={t("roles.passenger")}
+                  //right_component={<PassengerList futureReservedTrips={currentUser.futureReservedTripsUri} pastReservedTrips={currentUser.pastReservedTripsUri}/>}
+                  right_component={<PassengerList/>}
+                  left_title={t("roles.driver")}
+                  //left_component={<DriverList futureCreatedTrips={currentUser.futureCreatedTripsUri} pastCreatedTrips={currentUser.pastCreatedTripsUri}/>}
+                  left_component={<DriverList/>}
+              />
+          }
       </div>
     </div>
   );
