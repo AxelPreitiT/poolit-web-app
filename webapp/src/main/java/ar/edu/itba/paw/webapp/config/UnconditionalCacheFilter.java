88e5162330a221ec85fe7865facbea1cd7475290
JoseMenta <jmentasti@itba.edu.ar>
2024-01-17 15:31:13 -0300

    [BACK] fix: change UnconditionalCacheFilter mapping to use vite's location for static assets

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/UnconditionalCacheFilter.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/UnconditionalCacheFilter.java
@@ -8,13 +8,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.Duration;
-import java.time.Period;
-import java.time.temporal.ChronoUnit;
 
 public class UnconditionalCacheFilter extends OncePerRequestFilter {
 
     public static final long MAX_TIME = Duration.ofDays(365).getSeconds();
-
+//    https://developer.mozilla.org/es/docs/Web/HTTP/Headers/Cache-Control
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         response.setHeader("Cache-Control",String.format("public, max-age=%d, inmutable",MAX_TIME));
