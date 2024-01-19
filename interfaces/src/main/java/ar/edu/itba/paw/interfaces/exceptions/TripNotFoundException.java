be6682918fc4e00dca576e9a06e432d08be689d1
JoseMenta <jmentasti@itba.edu.ar>
2024-01-19 18:12:36 -0300

    [BACK] refactor: change exception mapping up to PassengerReviewService

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/exceptions/TripNotFoundException.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/exceptions/TripNotFoundException.java
@@ -2,15 +2,7 @@ package ar.edu.itba.paw.interfaces.exceptions;
 
 
 
-public class TripNotFoundException extends CustomException{
+public class TripNotFoundException extends Exception{
     private static final long serialVersionUID = 121312390745766939L;
 
-    private static final String MESSAGE_CODE = "exceptions.trip_not_found";
-    public TripNotFoundException(){
-        super(MESSAGE_CODE);
-    }
-
-    public TripNotFoundException(final int httpStatusCode){
-        super(MESSAGE_CODE,httpStatusCode);
-    }
 }
