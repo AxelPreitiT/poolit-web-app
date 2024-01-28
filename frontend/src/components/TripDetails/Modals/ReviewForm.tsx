d76bf8108ead6d688ecd13a581ef6b92a316d2a1
Gaston Francois <gfrancois@itba.edu.ar>
2024-01-28 06:01:25 -0300

    [FRONT] chore: add stars in modal

--- frontend/src/components/TripDetails/Modals/ReviewForm.tsx
+++ frontend/src/components/TripDetails/Modals/ReviewForm.tsx
@@ -8,11 +8,16 @@ import ReportsOptions from "@/enums/ReportsOptions.ts";
 const ReviewForm = () => {
     const { t } = useTranslation();
     const [selectedOption, setSelectedOption] = useState<ReportsOptions>(ReportsOptions.HARASSMENT);
+    const [selectedStarsOption, setSelectedStarsOption] = useState(1);
+
 
     const handleOptionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         setSelectedOption(e.target.value as ReportsOptions);
     };
 
+    const handleStarsOptionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedStarsOption(parseInt(e.target.value as string, 10));
+    };
 
     return (
         <div className={styles.picker_style}>
@@ -36,7 +41,19 @@ const ReviewForm = () => {
                 <label className="report-option-label">
                     <strong className="text">{t("reviews.rating")}</strong>
                 </label>
-
+                <FormSelect
+                    id="passenger-38"
+                    name="rating"
+                    className={styles.stars_option}
+                    value={selectedStarsOption}
+                    onChange={handleStarsOptionChange}
+                >
+                    <option value="1" className={styles.stars_option}>★</option>
+                    <option value="2" className={styles.stars_option}>★★</option>
+                    <option value="3" className={styles.stars_option}>★★★</option>
+                    <option value="4" className={styles.stars_option}>★★★★</option>
+                    <option value="5" className={styles.stars_option}>★★★★★</option>
+                </FormSelect>
             </div>
             <div>
                 <label className="report-option-label">
