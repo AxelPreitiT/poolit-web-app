ec61152cf5b9fed6390b37e2aaf2659da6d5d17b
Gaston Francois <gfrancois@itba.edu.ar>
2024-01-30 00:48:04 -0300

    [FRONT] fix: uris changes

--- frontend/src/hooks/passanger/useReviewsReportsDriver.tsx
+++ frontend/src/hooks/passanger/useReviewsReportsDriver.tsx
@@ -6,28 +6,31 @@ import {defaultToastTimeout} from "@/components/toasts/ToastProps.ts";
 import {parseTemplate} from "url-template";
 import PassangerService from "@/services/PassangerService.ts";
 import {useCurrentUser} from "@/hooks/users/useCurrentUser.tsx";
+import tripModel from "@/models/TripModel.ts";
 
-const useReviewsReportsDriver = (reporting:boolean, uri?: string) => {
+const useReviewsReportsDriver = (reporting:boolean, trip: tripModel) => {
     const { t } = useTranslation();
     const onQueryError = useQueryError();
     const {isLoading:isLoadingUser, data:currentUser} = useCurrentUser();
 
     const query = useQuery({
-        queryKey: ["driversReviews", uri, reporting],
+        queryKey: ["driversReviews", trip, reporting],
         queryFn: async () => {
             if (reporting){
-                const parseUri = parseTemplate(uri as string).expand({
+                console.log("ESTOY REPORT")
+                const parseUri = parseTemplate(trip.driverReportsUriTemplate as string).expand({
                     userId: currentUser?.userId as number,
                 });
                 return await PassangerService.getReport(parseUri);
             }else{
-                const parseUri = parseTemplate(uri as string).expand({
+                console.log("ESTOY REVIEW")
+                const parseUri = parseTemplate(trip.driverReviewsUriTemplate as string).expand({
                     userId: currentUser?.userId as number,
                 });
                 return await PassangerService.getReview(parseUri);
             }
         },
-        enabled: !!uri && !isLoadingUser,
+        enabled: !!trip && !isLoadingUser,
         retry: false
     });
 
