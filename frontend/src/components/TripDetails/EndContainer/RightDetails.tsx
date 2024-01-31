3e9954d0f35fa3786346ea95abd95250b7171939
Gaston Francois <gfrancois@itba.edu.ar>
2024-01-31 20:09:07 -0300

    [FRONT] chore: add modals in btn

--- frontend/src/components/TripDetails/EndContainer/RightDetails.tsx
+++ frontend/src/components/TripDetails/EndContainer/RightDetails.tsx
@@ -39,7 +39,7 @@ const BtnJoin = ( { trip }: { trip: tripModel }) => {
             () => {
                 onSubmitAccept();
                 invalidateTripState();}
-            }>
+        }>
             <div className={styles.create_trip_btn}>
                 <i className="bi bi-check-lg light-text"></i>
                 <span>{t("trip_detail.btn.join")}</span>
@@ -53,17 +53,45 @@ const BtnDelete = ( { uri, id }: { uri: string, id:number}) => {
     const {onSubmit:onSubmitDelete } = useDeleteTrip(uri, id);
     const navigate = useNavigate();
 
+    const [showModalDelete, setModalDelete] = useState(false);
+    const closeModalDelete = () => {setModalDelete(false);};
+    const openModalDelete = () => {setModalDelete(true);};
+
     return (
-        <Button className={styles.btn_cancel}
-                onClick={() => {
-                    onSubmitDelete();
-                    navigate(createdTripsPath);
-                }}>
-            <div className={styles.create_trip_btn}>
-                <i className="bi bi-x light-text"></i>
-                <span>{t("trip_detail.btn.delete")}</span>
-            </div>
-        </Button>
+        <div>
+            <Button className={styles.btn_cancel}
+                    onClick={() => openModalDelete()}>
+                <div className={styles.create_trip_btn}>
+                    <i className="bi bi-x light-text"></i>
+                    <span>{t("trip_detail.btn.delete")}</span>
+                </div>
+            </Button>
+
+            <Modal show={showModalDelete} onHide={closeModalDelete} aria-labelledby="contained-modal-title-vcenter" centered>
+                <div className={styles.propProfile}>
+                    <Modal.Header closeButton>
+                        <Modal.Title><h2 className={styles.titleModal}>{t('modal.delete_trip')}</h2></Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div className={styles.review_empty_container}>
+                            <i className={`bi-solid bi-exclamation-triangle-fill h2`}></i>
+                            <h3 className="italic-text placeholder-text">{t('trip_detail.btn.delete_warning')}</h3>
+                        </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button className={styles.backBtn} onClick={closeModalDelete}>
+                            {t('modal.Back')}
+                        </Button>
+                        <Button className={styles.cancelBtn} onClick={() => {
+                            onSubmitDelete();
+                            navigate(createdTripsPath);
+                            }}>
+                            {t("trip_detail.btn.delete")}
+                        </Button>
+                    </Modal.Footer>
+                </div>
+            </Modal>
+        </div>
     );
 }
 
@@ -72,18 +100,47 @@ const CancelBtn = ( { passanger }: { passanger?: passangerModel }) => {
     const {onSubmit:onSubmitCancel } = useCancelTrip(passanger?.selfUri as string);
     const navigate = useNavigate();
 
+    const [showModalCancel, setModalCancel] = useState(false);
+    const closeModalCancel = () => {setModalCancel(false);};
+    const openModalCancel = () => {setModalCancel(true);};
+
     return (
-        <Button className={styles.btn_cancel}
-                onClick={
-                    () => {
-                        onSubmitCancel()
-                        navigate(reservedTripsPath) ;}
-                    }>
-            <div className={styles.create_trip_btn}>
-                <i className="bi bi-x light-text"></i>
-                <span>{t("trip_detail.btn.cancel")}</span>
-            </div>
-        </Button>
+        <div>
+            <Button className={styles.btn_cancel}
+                    onClick={() => openModalCancel()}>
+                <div className={styles.create_trip_btn}>
+                    <i className="bi bi-x light-text"></i>
+                    <span>{t("trip_detail.btn.cancel")}</span>
+                </div>
+            </Button>
+
+            <Modal show={showModalCancel} onHide={closeModalCancel} aria-labelledby="contained-modal-title-vcenter" centered>
+                <div className={styles.propProfile}>
+                    <Modal.Header closeButton>
+                        <Modal.Title><h2 className={styles.titleModal}>{t('modal.cancel_trip')}</h2></Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div className={styles.review_empty_container}>
+                            <i className={`bi-solid bi-exclamation-triangle-fill h2`}></i>
+                            <h3 className="italic-text placeholder-text">{t('trip_detail.btn.cancel_warning')}</h3>
+                        </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button className={styles.backBtn} onClick={closeModalCancel}>
+                            {t('modal.Back')}
+                        </Button>
+                        <Button className={styles.cancelBtn} onClick={
+                            () => {
+                                onSubmitCancel()
+                                navigate(reservedTripsPath) ;}
+                        }>
+                            {t('modal.cancel')}
+                        </Button>
+                    </Modal.Footer>
+                </div>
+            </Modal>
+        </div>
+
     )
 }
 
@@ -127,88 +184,88 @@ const RightDetails = ({ isPassanger, isDriver, trip, passanger, status,  driver 
 
     return (
         (! isLoadingDiscovery && discovery != undefined && (
-        (!isPassanger && !isDriver) ?
-            <div className={styles.btn_container}>
-                <BtnJoin trip={trip}/>
-            </div> :
-            (status === Status.FINISHED ?
-                ( isDriver || passanger?.passengerState == passangerStatus.ACCEPTED ?
-                    <div className={styles.review_btn}>
-                        <div className={styles.btn_container}>
-                            <Button className={styles.btn_join} onClick={openModalReview}>
-                                <div className={styles.create_trip_btn}>
-                                    <i className="bi bi-pencil-square"></i>
-                                    <span>{t("trip_detail.btn.reviews")}</span>
-                                </div>
-                            </Button>
-                            <Link to={isDriver? createdTripsPath : reservedTripsPath} >
-                                <Button className={styles.btn_trips}>
-                                    <div className={styles.create_trip_btn}>
-                                        <i className="bi bi-car-front-fill light-text"></i>
-                                        <span>{t("trip_detail.btn.my_trips")}</span>
+            (!isPassanger && !isDriver) ?
+                <div className={styles.btn_container}>
+                    <BtnJoin trip={trip}/>
+                </div> :
+                (status === Status.FINISHED ?
+                        ( isDriver || passanger?.passengerState == passangerStatus.ACCEPTED ?
+                                <div className={styles.review_btn}>
+                                    <div className={styles.btn_container}>
+                                        <Button className={styles.btn_join} onClick={openModalReview}>
+                                            <div className={styles.create_trip_btn}>
+                                                <i className="bi bi-pencil-square"></i>
+                                                <span>{t("trip_detail.btn.reviews")}</span>
+                                            </div>
+                                        </Button>
+                                        <Link to={isDriver? createdTripsPath : reservedTripsPath} >
+                                            <Button className={styles.btn_trips}>
+                                                <div className={styles.create_trip_btn}>
+                                                    <i className="bi bi-car-front-fill light-text"></i>
+                                                    <span>{t("trip_detail.btn.my_trips")}</span>
+                                                </div>
+                                            </Button>
+                                        </Link>
                                     </div>
-                                </Button>
-                            </Link>
-                        </div>
-                        <div className={styles.report_link}>
-                            <span>{t('trip_detail.report.pre_link_text')}</span>
-                            <span onClick={openModalReport} style={{cursor: 'pointer', color:'blue'}}><i className="bi bi-car-front-fill"></i>{t('trip_detail.report.link_text')}</span>
-                        </div>
+                                    <div className={styles.report_link}>
+                                        <span>{t('trip_detail.report.pre_link_text')}</span>
+                                        <span onClick={openModalReport} style={{cursor: 'pointer', color:'blue'}}><i className="bi bi-car-front-fill"></i>{t('trip_detail.report.link_text')}</span>
+                                    </div>
 
-                        <Modal show={showModalReport} onHide={closeModalReport} aria-labelledby="contained-modal-title-vcenter" centered>
-                            <ModalReport openModalMake={openModalMake} closeModal={closeModalReport} driver={driver} isDriver={isDriver} trip={trip} passanger={passanger} reporting={true}/>
-                        </Modal>
+                                    <Modal show={showModalReport} onHide={closeModalReport} aria-labelledby="contained-modal-title-vcenter" centered>
+                                        <ModalReport openModalMake={openModalMake} closeModal={closeModalReport} driver={driver} isDriver={isDriver} trip={trip} passanger={passanger} reporting={true}/>
+                                    </Modal>
 
-                        <Modal show={showModalReview} onHide={closeModalReview} aria-labelledby="contained-modal-title-vcenter" centered>
-                            <ModalReport openModalMake={openModalMake} closeModal={closeModalReview} driver={driver} isDriver={isDriver} trip={trip} passanger={passanger} car={car} reporting={false} openModalCar={openModalCar}/>
-                        </Modal>
+                                    <Modal show={showModalReview} onHide={closeModalReview} aria-labelledby="contained-modal-title-vcenter" centered>
+                                        <ModalReport openModalMake={openModalMake} closeModal={closeModalReview} driver={driver} isDriver={isDriver} trip={trip} passanger={passanger} car={car} reporting={false} openModalCar={openModalCar}/>
+                                    </Modal>
 
-                        <Modal show={showModalMake} onHide={closeModalMake} aria-labelledby="contained-modal-title-vcenter" centered>
-                            <ModalMake closeModal={closeModalMake} user={userMake} reportForm={formMake}/>
-                        </Modal>
+                                    <Modal show={showModalMake} onHide={closeModalMake} aria-labelledby="contained-modal-title-vcenter" centered>
+                                        <ModalMake closeModal={closeModalMake} user={userMake} reportForm={formMake}/>
+                                    </Modal>
 
-                        <Modal show={showModalCar} onHide={closeModalCar} aria-labelledby="contained-modal-title-vcenter" centered>
-                            <ModalMakeCar closeModal={closeModalCar} car={car} reportForm={<ReviewCarForm/>}/>
-                        </Modal>
+                                    <Modal show={showModalCar} onHide={closeModalCar} aria-labelledby="contained-modal-title-vcenter" centered>
+                                        <ModalMakeCar closeModal={closeModalCar} car={car} reportForm={<ReviewCarForm/>}/>
+                                    </Modal>
 
-                    </div> :
-                    <div className={styles.btn_container}>
-                        <Link to={reservedTripsPath} >
-                            <Button className={styles.btn_trips}>
-                                <div className={styles.create_trip_btn}>
-                                    <i className="bi bi-car-front-fill light-text"></i>
-                                    <span>{t("trip_detail.btn.my_trips")}</span>
+                                </div> :
+                                <div className={styles.btn_container}>
+                                    <Link to={reservedTripsPath} >
+                                        <Button className={styles.btn_trips}>
+                                            <div className={styles.create_trip_btn}>
+                                                <i className="bi bi-car-front-fill light-text"></i>
+                                                <span>{t("trip_detail.btn.my_trips")}</span>
+                                            </div>
+                                        </Button>
+                                    </Link>
                                 </div>
-                            </Button>
-                        </Link>
-                    </div>
+                        )
+                        :
+                        (isDriver ?
+                                <div className={styles.btn_container}>
+                                    <Link to={createdTripsPath} >
+                                        <Button className={styles.btn_trips}>
+                                            <div className={styles.create_trip_btn}>
+                                                <i className="bi bi-car-front-fill light-text"></i>
+                                                <span>{t("trip_detail.btn.my_trips")}</span>
+                                            </div>
+                                        </Button>
+                                    </Link>
+                                    <BtnDelete uri={discovery?.tripsUriTemplate as string} id={trip.tripId}/>
+                                </div> :
+                                <div className={styles.btn_container}>
+                                    <Link to={reservedTripsPath} >
+                                        <Button className={styles.btn_trips}>
+                                            <div className={styles.create_trip_btn}>
+                                                <i className="bi bi-car-front-fill light-text"></i>
+                                                <span>{t("trip_detail.btn.my_trips")}</span>
+                                            </div>
+                                        </Button>
+                                    </Link>
+                                    <CancelBtn passanger={passanger}/>
+                                </div>
+                        )
                 )
- :
-                    (isDriver ?
-                        <div className={styles.btn_container}>
-                            <Link to={createdTripsPath} >
-                                <Button className={styles.btn_trips}>
-                                    <div className={styles.create_trip_btn}>
-                                        <i className="bi bi-car-front-fill light-text"></i>
-                                        <span>{t("trip_detail.btn.my_trips")}</span>
-                                    </div>
-                                </Button>
-                            </Link>
-                            <BtnDelete uri={discovery?.tripsUriTemplate as string} id={trip.tripId}/>
-                        </div> :
-                        <div className={styles.btn_container}>
-                            <Link to={reservedTripsPath} >
-                                <Button className={styles.btn_trips}>
-                                    <div className={styles.create_trip_btn}>
-                                        <i className="bi bi-car-front-fill light-text"></i>
-                                        <span>{t("trip_detail.btn.my_trips")}</span>
-                                    </div>
-                                </Button>
-                            </Link>
-                            <CancelBtn passanger={passanger}/>
-                            </div>
-                    )
-            )
         ))
     );
 };
