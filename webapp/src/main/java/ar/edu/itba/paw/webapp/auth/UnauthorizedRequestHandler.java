0eefa07f79c07e07000a9014b91afd232e65f5c2
JoseMenta <jmentasti@itba.edu.ar>
2024-02-02 19:52:55 -0300

    [BACK] chore: add header in unauthorized

--- webapp/src/main/java/ar/edu/itba/paw/webapp/auth/UnauthorizedRequestHandler.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/auth/UnauthorizedRequestHandler.java
@@ -32,5 +32,6 @@ public class UnauthorizedRequestHandler implements AuthenticationEntryPoint {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType(MediaType.APPLICATION_JSON);
         response.getWriter().write(String.format("{\n \"message:\" : \"%s\"\n}",messageSource.getMessage(MESSAGE_CODE,null, request.getLocale())));
+        response.setHeader("WWW-Authenticate","Bearer realm=\"Poolit\"");
     }
 }
