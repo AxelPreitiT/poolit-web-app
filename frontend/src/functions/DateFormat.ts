22db6e2eda342c3131abc49295911b9c200c032b
JoseMenta <jmentasti@itba.edu.ar>
2024-02-02 13:38:56 -0300

    [FRONT] fix: fix DateFormat function for case without time

--- frontend/src/functions/DateFormat.ts
+++ frontend/src/functions/DateFormat.ts
@@ -6,12 +6,17 @@ interface FormattedDateTime {
 const getFormattedDateTime = (
   dateTimeString: string | undefined
 ): FormattedDateTime => {
+  // if (!dateTimeString) {
+  //   return { date: '', time: '' };
+  // }
+  // const [date, time] = dateTimeString.split('T');
+  // return { date: date || '', time: time || '' };
   if (!dateTimeString) {
     return { date: "", time: "" };
   }
   const [date, time] = dateTimeString.split("T");
-  const [year, month, day] = date.split("-");
-  const [hour, minutes] = time.split(":");
+  const [year, month, day] = date? date.split("-") : ['','',''];
+  const [hour, minutes] = time? time.split(":") : ['',''];
 
   return {
     date: `${day}/${month}/${year}`,
