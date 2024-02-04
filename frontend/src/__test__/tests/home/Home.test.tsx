7594db69821c2c3b3b13bbf9742c88e2201db6d2
JoseMenta <jmentasti@itba.edu.ar>
2024-02-03 23:56:34 -0300

    [FRONT] test: add tests for home page

--- frontend/src/__test__/tests/home/Home.test.tsx
+++ frontend/src/__test__/tests/home/Home.test.tsx
@@ -1,12 +1,44 @@
 import {describe, expect} from "vitest";
-import {customRender} from "@/__test__/utils.tsx";
+import {customRender, screen} from "@/__test__/utils.tsx";
 import HomePage from "@/pages/home/HomePage.tsx";
 import setAuthToken from "@/__test__/tests/utils.ts";
+import {server} from "@/__test__/setup.ts";
+import UserMock from "@/__test__/mocks/UserMock.ts";
+import CityMock from "@/__test__/mocks/CityMock.ts";
+import CarFeatureMock from "@/__test__/mocks/CarFeatureMock.ts";
+import TripMock from "@/__test__/mocks/TripMock.ts";
 
 describe("Home",()=>{
-    it("Should show de tutorial page",()=>{
-        setAuthToken();
+    it("Should not show the tutorial page when not logged in ",async ()=>{
+        server.use(CityMock.getAllCities(),CarFeatureMock.getAllFeatures())
+
         customRender(<HomePage/>, {route:"/"})
-        expect(window.location.pathname).toBe("/");
+
+        expect(await screen.findByText(/Find your trip in CABA/i)).toBeVisible();
+        expect(await screen.findByText(/Book your trip/i)).toBeVisible();
+        expect(await screen.findByText(/Drive and share/i)).toBeVisible();
     });
+
+    it("Should not show the tutorial page when there are not recommended trips",async ()=>{
+        setAuthToken();
+        server.use(UserMock.getByIdPrivateRoleDriver(),CityMock.getAllCities(),CarFeatureMock.getAllFeatures(),TripMock.mockEmptyTrips())
+
+        customRender(<HomePage/>, {route:"/"})
+
+        expect(await screen.findByText(/Find your trip in CABA/i)).toBeVisible();
+        expect(await screen.findByText(/Book your trip/i)).toBeVisible();
+        expect(await screen.findByText(/Drive and share/i)).toBeVisible();
+    });
+
+    it("Should show the tutorial when there are recommended trips",async ()=>{
+        setAuthToken();
+        server.use(UserMock.getByIdPrivateRoleDriver(),CityMock.getAllCities(),CarFeatureMock.getAllFeatures(),TripMock.mockTripList())
+
+        customRender(<HomePage/>, {route:"/"})
+
+        expect(screen.queryByText(/Find your trip in CABA/i)).not.toBeInTheDocument();
+        expect(screen.queryByText(/Book your trip/i)).not.toBeInTheDocument();
+        expect(screen.queryByText(/Drive and share/i)).not.toBeInTheDocument();
+        expect(await screen.findByText(/Available trips/i)).toBeVisible();
+    })
 });
